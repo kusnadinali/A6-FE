f3d631b020b972c1fd8d7102b7f88959bc28bac1
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2021-10-31 01:16:36 +0700

    create navigasi bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, {useState,useEffect} from "react";
 import {View,Text ,ScrollView} from 'react-native';
 import Header from "./pages/Header";
+import Navbar from "./pages/Navbar";
 
 
 const App = ()=> {
   return (
     <View>
       <Header/>
-        <View style={{height:541}}>
-          
+        <View style={{backgroundColor:'grey',height:541}}>
           <ScrollView> 
           
           </ScrollView> 
         </View> 
 
-        <View style={{backgroundColor:'red',height:150}}>
-          
+        <View>
+          <Navbar/>
         </View>
     </View>
   );
---
+++ src/pages/Navbar/index.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { Image,StyleSheet, Text, View } from 'react-native'
+import bodynavbar from '../../assets/icons/body_navbar.png'
+import feeds from '../../assets/icons/feeds.png'
+import diamond from '../../assets/icons/diamond.png'
+import button_tengah from '../../assets/icons/button_tengah.png'
+import search from '../../assets/icons/search.png'
+import user from '../../assets/icons/user.png'
+
+const Navbar = () => {
+    return (
+        <View style={styles.wrapper}>
+            
+            <Image source={bodynavbar} style={styles.body} />
+            
+            <Image source={feeds} style={styles.feeds}/>
+            <Image source={search} style={styles.search}/>
+            <Image source={button_tengah} style={styles.midButton}/>
+            <Image source={diamond} style={styles.diamond}/>
+            <Image source={user} style={styles.user}/>
+            <Text style={styles.textFeed}>Feed</Text>
+            <Text style={styles.textSearch}>Search</Text>
+            <Text style={styles.textSafety}>Safety</Text>
+            <Text style={styles.textProfile}>Profile</Text>
+
+        </View>
+    )
+}
+
+export default Navbar;
+
+const styles = StyleSheet.create({
+    wrapper:{
+        backgroundColor: 'grey'
+    },
+    body: {
+        width: 360,
+        height: 60.56,
+    },
+    feeds:{
+        position: 'absolute',
+        width: 20,
+        height: 20,
+        left: 25.23,
+        top: 19.35,
+    },
+    search:{
+        position: 'absolute',
+        width: 20,
+        height: 20,
+        left: 94.73,
+        top: 17.66,
+    },
+    midButton:{
+        position: 'absolute',
+        width: 49.63,
+        height: 49.63,
+        left: 151.93,
+        top: 5.89,
+    },
+    diamond:{
+        position: 'absolute',
+        width: 19.35,
+        height: 19.35,
+        left: 233.51,
+        top: 16.82,
+    },
+    user:{
+        position: 'absolute',
+        width: 27.76,
+        height: 26.92,
+        left: 300.8,
+        top: 15.98,
+    },
+    textFeed:{
+        position: 'absolute',
+        width: 34.49,
+        height: 15,
+        fontFamily:'Roboto',
+        left:20,
+        top:40,
+        fontStyle:'normal',
+        fontWeight:'normal',
+        color:'#0C8EFF'
+    },
+    textSearch:{
+        position: 'absolute',
+        fontFamily:'Roboto',
+        left:87.2,
+        top:40,
+        fontStyle:'normal',
+        fontWeight:'normal',
+        color:'#9E9E9E'
+    },
+    textSafety:{
+        position: 'absolute',
+        fontFamily:'Roboto',
+        left:225,
+        top:40,
+        fontStyle:'normal',
+        fontWeight:'normal',
+        color:'#9E9E9E'
+    },
+    textProfile:{
+        position: 'absolute',
+        fontFamily:'Roboto',
+        left:297,
+        top:40,
+        fontStyle:'normal',
+        fontWeight:'normal',
+        color:'#9E9E9E'
+    }
+})
